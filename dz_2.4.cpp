fa6248682ad6563ccc0b5c0fba0879e229110d8a
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-09-23 16:58:44 +0300

    Update dz_2.4.cpp

--- dz_2.4.cpp
+++ dz_2.4.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
 	
 	
 	for (double i = -4.0; i <= 4; i += 0,5) {
-		cout <<"y=" << (pow(i,2)-2*i+2)/(i-1) << endl;
+		cout <<"y=" <<(I==1)?"vicolota": to_string((pow(i,2)-2*i+2)/(i-1)) << endl;
 	}
 
 }
