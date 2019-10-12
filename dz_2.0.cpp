f70de13fa46c220c319e1ed5f27c847d852c5344
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-12 13:17:57 +0300

    Update dz_2.0.cpp

--- dz_2.0.cpp
+++ dz_2.0.cpp
@@ -6,12 +6,12 @@ using namespace std;
 int main()
 {
     double h, R, r, l;
-    l=(2*M_PI*R);
+    
     cin >> R;
     cin >> r;
 
     cin >> h;
-
+l=(2*M_PI*R);
     cout << "V=" << (M_PI * h / 3 * (pow(R, 2) + (R * r) + pow(r, 2)));
     cout << "S=" << (M_PI * (pow(R, 2) + (R + r) * l + pow(r, 2)));
 }
