104094dcb4b391de9b1047b068de70a72e609366
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-12 14:09:08 +0300

    Update dz_2.1.cpp

--- dz_2.1.cpp
+++ dz_2.1.cpp
@@ -1,13 +1,16 @@
 #include <math.h>
 #include <iostream>
+
 using namespace std;
-int main()
-{
-	double x, a;
-	cin >> x;
-	cin >> a;
 
-	cout << "w=" << (abs(x) < 1 ? a * log(x) : sqrt(a - pow(x, 2)));
-
+int main() {
+    double x, a;
+    cin >> a;
+    cin >> x;
+    if  (a - pow(x, 2)>0){
+        cout << "w=" << (abs(x) < 1 ? a * log(abs(x)) : sqrt(a - pow(x, 2)));
+    }else{
+        cout<<"функция не определена при"<<"x="<<(x)<<"и"<<"a="<<(a);
+    }
 
 }
