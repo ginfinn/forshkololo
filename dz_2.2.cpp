c569cb8d3a1457bda2b3b23093a6a1813cd19f27
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-12 14:11:58 +0300

    Update dz_2.2.cpp

--- dz_2.2.cpp
+++ dz_2.2.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main() {
     double x, y, b;
     cin>>x>>y>>b;
-    
+
     if (sqrt(b - x) > 0&&log(b - y)>0)
-     {
+    {
         cout << "z=" << log(b - y) * sqrt(b - x);
     }else
-        cout<<"нет решений";
+        cout<<"функция не определена при данных значениях";
 }
