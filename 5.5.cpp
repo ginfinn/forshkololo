35ce2928c4339959d5a0b6aba6303497611842d4
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-21 10:19:28 +0300

    Update 5.5.cpp

--- 5.5.cpp
+++ 5.5.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	char A[5][7] = { {"  *   "},{"*   * "},{"     *"},{"      "},{"      "} };
+	char B[5][7] = { {"      "},{"      "},{"     *"},{"*   * "},{"  *   "} };
+	int n;
+	cin >> n;
+	for (int j = 0; j < 5; j++) {
+		if (j == 2)
+			cout << "*";
+		else
+			cout << " ";
+		for (int k = 0; k < n; k++) {
+			if (k % 2 == 0) {
+				for (int i = 0; i < 7; i++)
+					cout << A[j][i];
+			}
+			else {
+				for (int i = 0; i < 7; i++)
+					cout << B[j][i];
+			}
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
