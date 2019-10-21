f79d921c770f2910f49edd195110f1691feb9091
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-21 10:06:18 +0300

    Update dz_2.3.cpp

--- dz_2.3.cpp
+++ dz_2.3.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+
 using namespace std;
 int main()
 {
-	int n;
-	cin >> n;
-	for (int i = 0; i < 10; i++) {
-		cout << i + n << endl;
+	setlocale(0, "");
+	double N;
+	int i;
+	cout << "Введите N: ";
+	cin >> N;
+	int n = (int)(N+.5);
+	for (i = n; i < n + 10; i++) {
+		cout << i <<" ";
 	}
-     
+	system("pause");
+	return 0;
 }
