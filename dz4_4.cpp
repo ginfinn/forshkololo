ba463e7dd1fce16bb954d490974b8431ca7eeac4
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-27 20:14:30 +0300

    Create dz4_4.cpp

---
+++ dz4_4.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include<iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <stdlib.h>
+#define PI 3.14
+
+using namespace std;
+
+
+void main()
+{
+	for (int i = 0; i < 13; i++)
+	{
+		for (int j = 0; j < 24; j++)
+		{
+			if (i < 6 && j < 8)
+				cout << "*";
+			else
+				cout << "-";
+		}
+		cout << "\n";
+	}
+	}
+
+	
