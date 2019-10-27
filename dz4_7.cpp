8d0832d5c33b85e029e3f103af363ab99b6955fb
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-27 20:57:12 +0300

    Create dz4_7.cpp

---
+++ dz4_7.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include<iostream>
+#include <cmath>
+#include <vector>
+//#include "stdafx.h"
+#include <random>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <stdlib.h>
+
+#include <cstdio>
+#include <windows.h>
+//#include <dos.h>
+//#include<graphics.h>
+using namespace std;
+
+#define PI 3.14
+
+using namespace std;
+
+
+void main()
+{
+	int m = 37;
+	int i = 3;
+	int c = 64;
+	int s = 0;
+	for (int k = 0; k < 20; ++k)
+	{
+		s = (m * s + i) % c;
+		cout << s << " ";
+	}
+	}
