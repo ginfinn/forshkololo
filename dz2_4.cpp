be4b2ce6a593b91cac61c3456c6bf86cfc46d11d
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-10-27 21:07:56 +0300

    Create dz2_4.cpp

---
+++ dz2_4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double N;
+	cout << "Введите число N: ";
+	cin >> N;
+	if (N<0)
+	{for (int i = 0; i < 9; i++)
+
+
+	for (int i = 0; i < 9; i++)
+		cout << (abs(N+1))++ <<" ";
+	cout << "\n";
+}
