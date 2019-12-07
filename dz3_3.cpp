4c8fc4d21b306d9181d36ea8eb5c3052b05692bb
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-12-07 12:38:38 +0300

    Update dz3_3.cpp

--- dz3_3.cpp
+++ dz3_3.cpp
@@ -1,18 +1,23 @@
+#include <cstdlib>
 #include<iostream>
 #include <cmath>
 #include <vector>
+//#include "stdafx.h"
+#include <random>
 #include <fstream>
 #include <string>
 #include <algorithm>
-#include <cstdlib>
 #include <stdlib.h>
 
-
-using namespace std;
-void main()
+#include <cstdio>
+#include <windows.h>
+//#include <dos.h>
+//#include<graphics.h>
+using  namespace std;
+int main()
 {
 	ofstream myfile;
-	myfile.open("D:\\example.txt");
+	/*myfile.open("D:\\example.txt");
 	if (myfile.is_open())
 	{
 
@@ -20,15 +25,16 @@ void main()
 
 
 
-	}
+	}*/
 	myfile.close();
 	ifstream myfile1;
 	myfile1.open("D:\\example.txt");
 	if (myfile1.is_open()) {
 		string s;
 
-		getline(myfile1, s);
-		cout << s;
+		while (getline(myfile1, s)) {
+			cout << s << endl;
+		}
 
 	}
 }
