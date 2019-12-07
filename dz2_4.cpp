2db48351d608a7492d5d56174b5630c4c9e0e849
ginfinn <55462314+ginfinn@users.noreply.github.com>
2019-12-07 12:27:44 +0300

    Update dz2_4.cpp

--- dz2_4.cpp
+++ dz2_4.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-using namespace std;
+#include <math.h>
+using  namespace std;
 int main()
 {
-	double N;
-	cout << "Введите число N: ";
-	cin >> N;
-	if (N<0)
-	{for (int i = 0; i < 9; i++)
-
-
-	for (int i = 0; i < 9; i++)
-		cout << (abs(N+1))++ <<" ";
-	cout << "\n";
+	int a;
+	cin >> a;
+	a = abs(a);
+	for (int i = 0; i <= 10; i++) {
+		cout << a << endl;
+		a=a++;
+		
+	}
 }
